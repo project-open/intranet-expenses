5440e56ed5ea09955380cc1c472787b07802eb42
Frank Bergmann <frank.bergmann@project-open.com>
2013-11-06 19:12:23 +0000

    - Fixed l10n

--- www/index.tcl
+++ www/index.tcl
@@ -482,13 +482,13 @@ if {$add_hours_all_p} {
 
 append left_navbar_html "
 	<tr>
-	  <td class=form-label>Start Date</td>
+	  <td class=form-label>[_ intranet-core.Start_Date]</td>
 	  <td class=form-widget>
 	    <input type=textfield name=start_date value='$start_date'>
 	  </td>
 	</tr>
 	<tr>
-	  <td class=form-label>End Date</td>
+	  <td class=form-label>[_ intranet-core.End_Date]</td>
 	  <td class=form-widget>
 	    <input type=textfield name=end_date value='$end_date'>
 	  </td>
