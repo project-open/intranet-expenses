ea623f145218b3090db9f59ba08a60e7b8b18274
Frank Bergmann <frank.bergmann@project-open.com>
2017-02-05 16:02:28 +0000

    - Integrated upgrade scripts

--- sql/postgresql/intranet-expenses-create.sql
+++ sql/postgresql/intranet-expenses-create.sql
@@ -526,3 +526,23 @@ from 	im_categories
 where 	category_type = 'Intranet Expense Payment Type'
 	and (enabled_p is null OR enabled_p = 't');
 
+
+
+
+SELECT im_component_plugin__new (
+	null,					-- plugin_id
+	'im_component_plugin',			-- object_type
+	now(),					-- creation_date
+	null,					-- creation_user
+	null,					-- creation_ip
+	null,					-- context_id
+	'Expense Bundle Filestorage',		-- plugin_name
+	'intranet-filestorage',			-- package_name
+	'bottom',				-- location
+	'/intranet-expenses/bundle-new',	-- page_url
+	null,					-- view_name
+	30,					-- sort_order
+	'im_filestorage_cost_component $user_id $bundle_id $bundle_name $return_url' -- component_tcl
+);
+
+
